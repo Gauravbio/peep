3134d07c5dcdb09aa9d69368d627a30fe37ddc65
Gauravbio <93136782+Gauravbio@users.noreply.github.com>
2023-04-26 12:53:39 +0530

    text

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -17,7 +17,7 @@ const SidebarItem:React.FC<SidebarItemProps> = ({
         <div className="relative  rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
             <Icon size={28} color='white' />
         </div>
-        <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer">
+        <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer text-white" >
             <Icon size={24} color='white' />
             <p>{label}</p>
         </div>
